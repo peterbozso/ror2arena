381bdb97d2b5bfe264734d152d515986434725e6
Péter Bozsó <peter.bozso@outlook.com>
2022-04-07 17:11:42 +0200

    Remove unnecessary attribute

--- src/Arena/ArenaPlugin.cs
+++ src/Arena/ArenaPlugin.cs
@@ -1,5 +1,4 @@
 ﻿using BepInEx;
-using R2API;
 using R2API.Utils;
 using RoR2;
 
@@ -7,8 +6,6 @@ namespace Arena;
 
 // This attribute specifies that we have a dependency on R2API, as we're using it to add our item to the game.
 [BepInDependency(R2API.R2API.PluginGUID)]
-// We will be using 2 modules from R2API: ItemAPI to add our item and LanguageAPI to add our language tokens.
-[R2APISubmoduleDependency(nameof(ItemAPI), nameof(LanguageAPI))]
 // This attribute is required, and lists metadata for the plugin.
 [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
 
