b80257fc29521f1b59eb9c94f9eefd9f7568a661
Péter Bozsó <peter.bozso@outlook.com>
2022-04-08 14:38:40 +0200

    Add debug logs

--- src/Arena/Managers/Champion.cs
+++ src/Arena/Managers/Champion.cs
@@ -9,12 +9,12 @@ public class Champion
     {
         get
         {
-            var players = PlayerCharacterMasterController.instances;
-            var alivePlayers = players.Where(player => IsAlive(player.master)).ToArray();
+            var players = CharacterMaster.readOnlyInstancesList;
+            var alivePlayers = players.Where(player => IsAlive(player)).ToArray();
 
             if (alivePlayers.Length == 1)
             {
-                return alivePlayers[0].master.GetBody().GetUserName();
+                return alivePlayers[0].GetBody().GetUserName();
             }
 
             return string.Empty;
@@ -25,8 +25,17 @@ public class Champion
     {
         var body = player.GetBody();
 
-        return (body && body.healthComponent.alive)
-            || player.inventory.GetItemCount(RoR2Content.Items.ExtraLife) > 0
-            || player.inventory.GetItemCount(DLC1Content.Items.ExtraLifeVoid) > 0;
+        var bodyIsAlive = body && body.healthComponent.alive;
+        var extraLifeCount = player.inventory.GetItemCount(RoR2Content.Items.ExtraLife);
+        var extraLifeVoidCount = player.inventory.GetItemCount(DLC1Content.Items.ExtraLifeVoid);
+        var isAlive = bodyIsAlive || extraLifeCount > 0 || extraLifeVoidCount > 0;
+
+        Log.LogDebug(player.GetBody().GetUserName());
+        Log.LogDebug("body is " + (bodyIsAlive ? "alive" : "dead"));
+        Log.LogDebug("ExtraLife count: " + player.inventory.GetItemCount(RoR2Content.Items.ExtraLife));
+        Log.LogDebug("ExtraLifeVoid count: " + player.inventory.GetItemCount(DLC1Content.Items.ExtraLifeVoid));
+        Log.LogDebug("summary: player is " + (isAlive ? "alive" : "dead"));
+
+        return isAlive;
     }
 }
