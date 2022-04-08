2976dd64944873e5d4a49cba06901ed98c50f84a
Péter Bozsó <peter.bozso@outlook.com>
2022-04-08 07:09:54 +0200

    Remove unnecessary comments

--- src/Arena/ArenaPlugin.cs
+++ src/Arena/ArenaPlugin.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 
 namespace Arena;
 
-// This attribute specifies that we have a dependency on R2API, as we're using it to add our item to the game.
 [BepInDependency(R2API.R2API.PluginGUID)]
-// This attribute is required, and lists metadata for the plugin.
 [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-
 public class ArenaPlugin : BaseUnityPlugin
 {
     public const string PluginGUID = PluginAuthor + "." + PluginName;
@@ -17,7 +14,6 @@ public class ArenaPlugin : BaseUnityPlugin
     public const string PluginName = "Arena";
     public const string PluginVersion = "0.0.1";
 
-    // The Awake() method is run at the very start when the game is initialized.
     public void Awake()
     {
         Log.Init(Logger);
@@ -61,7 +57,6 @@ public class ArenaPlugin : BaseUnityPlugin
         ArenaManager.FriendlyFire.Disable();
     }
 
-    // The Update() method is run on every frame of the game.
     private void Update()
     {
 #if DEBUG
