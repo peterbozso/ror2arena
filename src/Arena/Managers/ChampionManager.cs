96c47d568d4b3f66050923620182e0e685c59d57
Péter Bozsó <peter.bozso@outlook.com>
2022-04-09 14:00:35 +0200

    Refactor ChampionManager

--- src/Arena/Managers/ChampionManager.cs
+++ src/Arena/Managers/ChampionManager.cs
@@ -10,21 +10,18 @@ public class ChampionManager
         get
         {
             var players = PlayerCharacterMasterController.instances;
+
             var alivePlayers = players.Where(player => IsAlive(player.master)).ToArray();
 
-            if (alivePlayers.Length == 1)
-            {
-                return alivePlayers[0].master.GetBody().GetUserName();
-            }
-
-            return string.Empty;
+            return alivePlayers.Length == 1
+                ? alivePlayers[0].master.GetBody().GetUserName()
+                : string.Empty;
         }
     }
 
     private static bool IsAlive(CharacterMaster player)
     {
         var body = player.GetBody();
-
         var bodyIsAlive = body && body.healthComponent.alive;
         var extraLifeCount = player.inventory.GetItemCount(RoR2Content.Items.ExtraLife);
         var extraLifeVoidCount = player.inventory.GetItemCount(DLC1Content.Items.ExtraLifeVoid);
