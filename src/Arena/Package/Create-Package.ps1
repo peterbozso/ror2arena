344cef6d79487b4badfb734412df0c3d74fd9918
Péter Bozsó <peter.bozso@outlook.com>
2022-04-27 10:06:03 +0200

    Refactor folder deletion

--- src/Arena/Package/Create-Package.ps1
+++ src/Arena/Package/Create-Package.ps1
@@ -6,7 +6,6 @@ $PackagePath = 'package'
 $ZipPath = "$(Get-Location)\$ModName.zip"
 
 dotnet build ..\..\$ModName.sln /warnaserror --no-incremental --configuration Release --output $OutputPath
-
 if (!$?) {
     exit 1
 }
@@ -15,10 +14,8 @@ New-Item $PackagePath -ItemType Directory | Out-Null
 
 Copy-Item "$OutputPath\$ModName.dll", 'icon.png', 'README.md', 'manifest.json' -Destination $PackagePath
 
-Remove-Item $OutputPath -Recurse
-
 Compress-Archive $PackagePath\* -DestinationPath $ZipPath -Force
 
-Remove-Item $PackagePath -Recurse
+Remove-Item $OutputPath, $PackagePath -Recurse
 
 Write-Host "`nPackage is successfully created at: $ZipPath" -ForegroundColor Green
